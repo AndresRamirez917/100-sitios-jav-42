c687a5d22e519afcd9d4242922db51926e7222fe
AndresRamirez917 <andresmintic2021@gmail.com>
2024-10-09 19:23:44 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,61 @@
+async function getData() {
+    const result = await fetch('https://randomuser.me/api?results=2');
+    const ranUser = await result.json();
+    console.log(ranUser)
+    ranUser.results.forEach(element => {
+        const img = document.createRange().createContextualFragment(`
+            
+            <div class="card card-1">
+                    <img src="${element.picture.large}" alt="">
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur deleniti nisi, non saepe eaque vel sed labore in fugiat ipsa obcaecati reprehenderit debitis itaque cumque harum consequatur ipsam veniam quaerat!</p>
+                </div>
+            
+            `)
+            const card = document.querySelector('.opinions-row');
+ 
+            card.append(img)
+  
+    });
+}
+
+const btn_validar = document.getElementById('btn-validar');
+const validar = (e) => {
+e.preventDefault();
+const nombre = document.getElementById('nombre');
+const email = document.getElementById('email');
+const mensaje = document.getElementById('mensaje');
+const arr = [];
+const messageArr = ["Nombre", "Email", "Mensaje"];
+arr.push(nombre, email, mensaje);
+for(i = 0; i < arr.length; i++){
+    if(arr[i].value == ""){
+        swal({
+            title: `El campo ${messageArr[i]} no puede estar vacío`,
+            icon: "error",
+             })
+             return false;
+    }
+}
+if(!emailValido(email)){
+    swal({
+        title: `El campo ${messageArr[1]} no tiene el formato adecuado`,
+        icon: "error",
+         })
+         return false;
+}
+swal({
+    title: `Datos enviados satisfactoriamente`,
+    icon: "success",
+     })
+     nombre.value = "";
+     email.value = ""; 
+     mensaje.value = "";
+return true;
+}
+
+const emailValido = (email) => {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value)
+}
+
+getData()
+btn_validar.addEventListener("click", validar); 
